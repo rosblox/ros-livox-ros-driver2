5fd9c78d547ecba84b5875f7cc326af68530fe10
Max Polzin <polzin.max@gmail.com>
2023-11-28 14:20:14 +0100

    Update submodule.

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ REPOSITORY_NAME="$(basename "$(dirname -- "$( readlink -f -- "$0"; )")")"
 
 docker run -it --rm \
 --network=host \
---ipc=host --pid=host \
+--ipc=host \
+--pid=host \
 --env UID=$(id -u) \
 --env GID=$(id -g) \
 --volume $(pwd)/livox_ros_driver2:/colcon_ws/src/livox_ros_driver2 \
